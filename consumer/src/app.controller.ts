e761d88017508770145324b900236b6d6d11bbc9
宇賀持 達哉 <tugaji@gladd.jp>
2025-04-30 00:18:02 +0900

    kafka consumerの実装

--- consumer/src/app.controller.ts
+++ consumer/src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Payload, Ctx, KafkaContext, MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('app.public.user')
+  handleMessage(@Payload() message: any, @Ctx() context: KafkaContext) {
+    console.log('kafka consumer Received message', message);
+    // メッセージの処理ロジック
   }
 }
--- consumer/src/main.ts
+++ consumer/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // consumerの起動
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['broker1:9092', 'broker2:9093'],
+      },
+      consumer: {
+        groupId: 'app_consumer-group',
+      },
+    },
+  });
+  await app.startAllMicroservices();
+  console.log('Kafka Consumer is starting...');
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
